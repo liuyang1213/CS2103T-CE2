07864fc94a3c0da764c99113bfcafa5a6d828e11
liuyang <melody9951213@gmail.com>
2015-09-19 16:29:17 +0800

    added test cases for search, and it fails

--- src/JUnitTextBuddyAtd.java
+++ src/JUnitTextBuddyAtd.java
@@ -24,6 +24,34 @@ public class JUnitTextBuddyAtd {
 		testCommand("sort", "sample.txt is empty");	
 	}
 	
+	@Test
+	public void testSearch() {
+		String[] args = { "sample.txt" };
+		
+		testPreProcess(args);
+		
+		testCommand("search line", "1. first line\n2. second line"
+				    + "\n3. third line\n4. forth line");	
+	}
+
+	@Test
+	public void testSearchOneResult() {
+		String[] args = { "sample.txt" };
+		
+		testPreProcess(args);
+		
+		testCommand("search first", "1. first line");	
+	}
+	
+	@Test
+	public void testSearchNoResult() {
+		String[] args = { "sample.txt" };
+		
+		testPreProcess(args);
+		
+		testCommand("search blabla", "No item contains \"blabla\"");	
+	}
+	
 	private void testCommand(String command, String expected) {
 		String feedback = TextBuddy.executeCommand(command);
 		assertEquals(expected, feedback);
